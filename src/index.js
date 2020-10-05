4ca6bc8abba8c54b54e9110e436b578bf86364ea
Victor Avdeev <avdeevvictor95@gmail.com>
2020-10-05 06:17:09 +0300

    feat: add realization of check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var strRegEx = "";
+    for (var i = 0; i < bracketsConfig.length; i++){
+      if (i>0)
+        strRegEx = `${strRegEx}|`;
+      for (var j = 0 ; j<= 1 ; j++){
+        if ( /^\w{1}$/.test(bracketsConfig[i][j]) )
+          strRegEx = `${strRegEx}${bracketsConfig[i][j]}`;
+        else
+          strRegEx = `${strRegEx}\\${bracketsConfig[i][j]}`;
+      }
+    }
+    var regEx = new RegExp(strRegEx);
+    while (str.search(regEx) !== -1){
+      str = str.replace(regEx, "");
+    }
+    return str.length === 0;
+  }
